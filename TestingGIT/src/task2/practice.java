0ea912b6780d71825d441416742c6ced159727f3
admin <admin@Roshani>
2021-07-30 15:58:25 +0530

    changes succuful

--- TestingGIT/src/task2/practice.java
+++ TestingGIT/src/task2/practice.java
@@ -3,7 +3,9 @@ package task2;
 public class practice {
 	public void m1()
 	{
+		System.out.println("m1 method start");
 		System.out.println("m1 method");
+		System.out.println("method end");
 	}
 	
 public static void main(String[] args)
