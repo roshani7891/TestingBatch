e34a27c8396eb8edc9cdac2ab84863e1669f575f
admin <admin@Roshani>
2021-07-30 19:50:21 +0530

    by pras

---
+++ TestingGIT/src/gitstask/Git12.java
@@ -0,0 +1,16 @@
+package gitstask;
+
+public class Git12
+{
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	public void m1()
+	{
+		System.out.println("git example");
+	}
+
+}
---
+++ git/src/com/git/git.java
@@ -0,0 +1,21 @@
+package com.git;
+
+public class git
+{
+	
+	public void add()
+	{
+	int a=20,b=30,sum=0;
+	
+	  sum=a+b;
+	  System.out.println("Addition of a and b= "+sum);
+	 
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		
+		git g=new git();
+		g.add();
+	}
+}
