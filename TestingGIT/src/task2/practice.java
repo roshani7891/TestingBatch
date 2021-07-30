b26c750b694655d4a8d59bc747d28d4bd57a56cb
admin <admin@Roshani>
2021-07-30 15:39:13 +0530

    new class

---
+++ TestingGIT/src/task2/practice.java
@@ -0,0 +1,14 @@
+package task2;
+
+public class practice {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	
+public static void main(String[] args)
+{
+	practice p=new practice();
+	p.m1();
+}
+}
